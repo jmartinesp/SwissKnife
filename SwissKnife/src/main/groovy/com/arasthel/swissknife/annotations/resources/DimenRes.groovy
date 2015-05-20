1ceec479f7517869a461d2422431c39527bc9f2e
mrbimc <pavel.sikun@gmail.com>
2015-05-20 21:27:02 +0300

    Created 10 new res-injecting annotations and transformator for them.

---
+++ SwissKnife/src/main/groovy/com/arasthel/swissknife/annotations/resources/DimenRes.groovy
@@ -0,0 +1,18 @@
+package com.arasthel.swissknife.annotations.resources
+
+import org.codehaus.groovy.transform.GroovyASTTransformationClass
+
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
+/**
+ * Created by MrBIMC on 18/05/15.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.FIELD)
+@GroovyASTTransformationClass(["com.arasthel.swissknife.annotations.resources.ResTransformation"])
+public @interface DimenRes {
+    int value() default -1;
+}
