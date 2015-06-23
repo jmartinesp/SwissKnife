9d32a28befa3915c2c692fd412212f79fd831d33
Andrew Reitz <aj.reitz@gmail.com>
2015-06-23 10:47:44 -0500

    Setting up tests

--- SwissKnife/src/androidTest/groovy/com/arasthel/swissknife/annotations/ParcelableTestSpec.groovy
+++ SwissKnife/src/androidTest/groovy/com/arasthel/swissknife/annotations/ParcelableTestSpec.groovy
@@ -1,19 +1,19 @@
 package com.arasthel.swissknife.annotations
 
-import android.os.Parcel;
+import android.os.Parcel
 
 /**
  * Created by Arasthel on 23/6/15.
  */
 
-import spock.lang.Specification;
+import spock.lang.Specification
 
 public class ParcelableTestSpec extends Specification {
 
     def "parcel String works"() {
         given:
             def string = "this is a test"
-            def parcel = new Parcel()
+            def parcel = Parcel.obtain()
 
         when:
             parcel.writeValue(string)
